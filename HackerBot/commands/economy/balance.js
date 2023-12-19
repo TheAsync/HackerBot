fde74d3673de6d90caff936945ad0e18549d12e3
Viktor Fønlev <66299649+foenlev@users.noreply.github.com>
2023-12-19 02:41:54 +0100

    basic balance command

---
+++ HackerBot/commands/economy/balance.js
@@ -0,0 +1,20 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName('balance')
+    .setDescription('hard earned cash will be shown ;)'),
+
+  async execute(interaction) {
+
+    await interaction.deferReply({ ephemeral: true });
+    interaction.editReply({
+        content: "", embeds: [new EmbedBuilder()
+            .setTimestamp()
+            .setColor('Green')
+            .setDescription('You have **' + ' coins**')
+            .setTitle('Your Balance')]
+    });
+	},
+};
