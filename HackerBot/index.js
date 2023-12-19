75f3c9dc1746dd80757e1bde4308f226494d2029
Viktor <66299649+VikDk@users.noreply.github.com>
2023-12-19 22:50:00 +0100

    Update index.js

--- HackerBot/index.js
+++ HackerBot/index.js
@@ -6,10 +6,10 @@ console.log("[HACKERBOT] starting...\n");
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "mydb"
+  host: config.db.host,
+  user: config.db.user,
+  password: config.db.password,
+  database: "discord"
 });
 
 con.connect(function(err) {
@@ -39,10 +39,8 @@ client.cooldowns = new Collection();
 global.progressBar = (value, maxValue, size) => {
   const percentage = value / maxValue; // Calculate the percentage of the bar
   const progress = Math.round((size * percentage)); // Calculate the number of square caracters to fill the progress side.
-  const emptyProgress = size - progress; // Calculate the number of dash caracters to fill the empty progress side.
-
   const progressText = '/'.repeat(progress); // Repeat is creating a string with progress * caracters in it
-  const emptyProgressText = '—'.repeat(emptyProgress); // Repeat is creating a string with empty progress * caracters in it
+  const emptyProgressText = '—'.repeat(size - progress); // Repeat is creating a string with empty progress * caracters in it
   const percentageText = Math.round(percentage * 100) + '%'; // Displaying the percentage of the bar
 
   return '```[' + progressText + emptyProgressText + '] ' + percentageText + '```'; // Creating the bar
